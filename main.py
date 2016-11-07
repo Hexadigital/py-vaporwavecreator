c05bc77568f9a46596b659b6edb0821dea46a333
Hexadigital <hexadigital@vastitune.com>
2016-11-07 15:02:32 -0500

    Started work on the VaporwaveCreator class

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import sys
+import os, sys
+
+class VaporwaveCreator:
+	def __init__(self, filename):
+		if os.path.isfile(filename):
+			self._filename = filename
+		else:
+			raise IOError("The file specified could not be found.")
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
@@ -7,5 +14,6 @@ if __name__ == "__main__":
 			filename += sys.argv[i] + " "
 		filename = filename[:-1]
 		print(filename)
+		vw = VaporwaveCreator(filename)
 	else:
 		print("This script requires the input file name as an argument if run directly.")
